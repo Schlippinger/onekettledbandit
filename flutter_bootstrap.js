a6ac8d0c3950e26a1b76199a74c1a560f7a01ae4
Schlippinger <schlippediwipp@googlemail.com>
2026-06-23 16:03:17 +0000

    Deploying to gh-pages from @ Schlippinger/onekettledbandit@3f053ecddca82b52e87deb2b778f0e7696abcab0 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a4ce257c68517c1410f4b48ac9852ab5642a3f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1183790439" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3695354412" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
